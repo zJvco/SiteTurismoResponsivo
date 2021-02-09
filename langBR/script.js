e99d3ab83f601b859986d71f8299dc05536b86f0
Jvco <65981077+zJvco@users.noreply.github.com>
2021-02-09 16:23:56 -0300

    pagina principal pronta

---
+++ langBR/script.js
@@ -0,0 +1,53 @@
+// Idioma
+const btn = document.querySelector('.seta')
+const langUSA = document.querySelector('.img-EUA')
+
+btn.addEventListener('click', () => {
+    if (langUSA.style.display == 'block') {
+        langUSA.style.display = 'none'
+    }
+    else {
+        langUSA.style.display = 'block'
+    }
+})
+
+// Slider
+function loadSlider() {
+    const sliderImagens = document.querySelector('.imagens')
+    const imagens = document.querySelectorAll('.imagens img')
+
+    const btnEsquerda = document.querySelector('.seta-esquerda')
+    const btnDireita = document.querySelector('.seta-direita')
+
+    let counter = 1
+    const size = imagens[0].clientWidth;
+
+    sliderImagens.style.transform = 'translateX(' + (-size * counter) + 'px)'
+
+    btnDireita.addEventListener('click', () => {
+        if (counter >= imagens.length - 1) return
+        sliderImagens.style.transition = 'transform 0.4s ease-in-out'
+        counter++
+        sliderImagens.style.transform = 'translateX(' + (-size * counter) + 'px)'
+    })
+
+    btnEsquerda.addEventListener('click', () => {
+        if (counter <= 0) return
+        sliderImagens.style.transition = 'transform 0.4s ease-in-out'
+        counter--
+        sliderImagens.style.transform = 'translateX(' + (-size * counter) + 'px)'
+    })
+
+    sliderImagens.addEventListener('transitionend', () => {
+        if (imagens[counter].id === 'ultimaImg') {
+            sliderImagens.style.transition = 'none'
+            counter = imagens.length - 2
+            sliderImagens.style.transform = 'translateX(' + (-size * counter) + 'px)'
+        }
+        if (imagens[counter].id === 'primeiraImg') {
+            sliderImagens.style.transition = 'none'
+            counter = imagens.length - counter
+            sliderImagens.style.transform = 'translateX(' + (-size * counter) + 'px)'
+        }
+    })
+}
